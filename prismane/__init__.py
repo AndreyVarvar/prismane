4cb8c00c7a46c505709d17e3cf2e27c828f65064
Andrey Varvaryuk <andrey.varvaryuk@gmail.com>
2026-03-23 14:41:55 +0200

    I officially have no clue how I fixed it

---
+++ prismane/__init__.py
@@ -0,0 +1,5 @@
+from prismane.engine import Engine
+from prismane.scene import Scene
+from prismane.object import Object, ObjectArray
+from prismane.components import Component
+from prismane.panels import MasterControlPanel
